7246de98149936ad49c9f71490d3a913bfa81a1d
xiaocdon <15652914852@163.com>
2017-05-04 15:39:15 -0700

    added time filter function

--- frontend/src/js/ui/backup/view/backup.databases.js
+++ frontend/src/js/ui/backup/view/backup.databases.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Datetime from 'react-datetime'
 import BackupDatabase from './backup.database';
+import input from '../../../utility/input';
 
 
 export default class BackupDatabases extends Component {
@@ -8,6 +9,12 @@ export default class BackupDatabases extends Component {
     constructor(props) {
         super(props);
         this.dbToggleMap = {};
+        this.timeFilters = {
+            startTime: null,
+            endTime: null
+        };
+        this.copyDBs = props.copyDBs;
+        this.filteredDBs = this.copyDBs;
     }
 
     toggleDatabase(dbName) {
@@ -18,8 +25,64 @@ export default class BackupDatabases extends Component {
         this.forceUpdate();
     }
 
+    onTimeChange(key, value) {
+        this.timeFilters[key] = value;
+        this.search();
+    }
+
+    search() {
+        let filteredDBs = this.copyDBs;
+        const { startTime, endTime } = this.timeFilters;
+        if(!input.isEmpty(startTime)) {
+            try {
+                filteredDBs = filteredDBs.filter(db => {
+                    return startTime.isSameOrBefore(new Date(db.createdTime))
+                })
+            }catch (e) {console.error(e)}
+        }
+
+        if(!input.isEmpty(endTime)) {
+            try {
+                filteredDBs = filteredDBs.filter(db => {
+                    return endTime.isSameOrAfter(new Date(db.createdTime));
+                })
+            }catch (e) {}
+        }
+
+        this.filteredDBs = filteredDBs;
+        this.forceUpdate();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.copyDBs = nextProps.copyDBs;
+        this.search();
+    }
+
+    resetFilter(event) {
+        event.preventDefault();
+        this.timeFilters.startTime = null;
+        this.timeFilters.endTime = null;
+        this.search();
+    }
+
     render() {
-        const copyDBs = this.props.copyDBs;
+        const copyDBs = this.filteredDBs;
+        const { startTime, endTime } = this.timeFilters;
+
+       // console.log(this.copyDBs, copyDBs);
+        const startTimeProps = {};
+        if(startTime) {
+            startTimeProps.defaultValue = startTime
+        }else {
+            startTimeProps.value = "";
+        }
+
+        const endTimeProps = {};
+        if(endTime) {
+            endTimeProps.defaultValue = endTime
+        }else {
+            endTimeProps.value = "";
+        }
 
         return (
             <div className="backup-databases">
@@ -30,14 +93,24 @@ export default class BackupDatabases extends Component {
                     <div className="filter">
                         <div className="time-selector">
                             <span className="name">From</span>
-                            <div className="selector"><Datetime/></div>
+                            <div className="selector">
+                                <Datetime
+                                    { ...startTimeProps }
+                                    onChange={ this.onTimeChange.bind(this, 'startTime') }
+                                />
+                            </div>
                         </div>
                         <div className="time-selector">
                             <span className="name">To</span>
-                            <div className="selector"><Datetime/></div>
+                            <div className="selector">
+                                <Datetime
+                                    { ...endTimeProps }
+                                    onChange={ this.onTimeChange.bind(this, 'endTime') }
+                                />
+                            </div>
                         </div>
-                        <span className="filter-button">
-                            filter
+                        <span className="reset-button clickable" onClick={ this.resetFilter.bind(this) }>
+                            reset
                         </span>
                     </div>
                 </div>
