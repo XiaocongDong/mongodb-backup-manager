123fff996d6b4089b35fcfc2c8124aa6fedc03f6
Xiaocong Dong <15652914852@163.com>
2017-07-16 18:03:01 -0700

    strip comments

--- server/modules/config/index.js
+++ server/modules/config/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const stripJsonComments = require('strip-json-comments');
 
 let config = fs.readFileSync(path.join(__dirname, "../backup.config.json"), "utf-8");
-config = JSON.parse(config);
+config = JSON.parse(stripJsonComments(config));
 
 module.exports = config;
