45ced80798e447950ccb6328fc884b24d68ab73b
Xiaocong Dong <15652914852@163.com>
2017-07-16 17:10:59 -0700

    added package.json to the root

--- server/modules/router/frontend.js
+++ server/modules/router/frontend.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 
-const publicPath = express.static('../frontend/dist');
-const indexFile = path.join(__dirname, '../../../frontend/dist/index.html');
+const publicPath = express.static(path.join(__dirname, '../frontend/dist'));
+const indexFile = path.join(__dirname, '../frontend/dist/index.html');
 
 module.exports = {
     publicPath,
